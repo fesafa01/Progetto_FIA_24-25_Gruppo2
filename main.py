51d222ee98792b03f4b8a59f0dfd43487f6ea2df
beabussone <beatricebussone@hotmail.com>
2025-02-01 18:31:38 +0100

    Realizzazione pattern strategy
    Realizzazione di una classe astratta ModelEvaluation che implementa i tre metodi astratti splitter, run ed evaluate. Le classi che implementano quella astratta sono Holdout, LeaveOneOut, RandomSubsampling. Implementazione della factory per gestire lo strategy pattern. Modifica del main per la selezione del modello di validazione.

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from preprocessing import Preprocessing
 from model_selection import ModelSelection as ms
 import random
 from classificatore_KNN import classificatore_KNN 
-from Holdout import Holdout
-from leave_one_out import leave_one_out
-from RandomSubsampling import RandomSubsampling
 from metrics_calculator import metrics_calculator
+from ModelEvaluation import ModelEvaluationFactory
 
 # Definiamo il nome del file su cui lavorare
 filename = "version_1.csv"
@@ -45,37 +43,9 @@ print(y)
 #Scelgo il metodo di divisione del dataset in train e test set
 choice = ms.model_selection()
 
-if choice == "1":
-    # Metodo Holdout
-    test_size = float(input("Inserisci il valore percentuale che rappresenta la dimensione del test set rispetto all'intero set di dati: "))
-    k = int(input("Inserisci il valore di k pari al numero di vicini da considerare: "))
-    holdout = Holdout(test_size)
-    metriche = holdout.evaluate(X,y,k)  #Calcoliamo tutte le metriche
-    metriche_da_stampare = metrics_calculator() # Inizializzazione
-    metriche_da_stampare.stampa_metriche(metriche) # Chiamata al metodo che stampa solo le metriche desiderate
-    
-
-elif choice == "2":
-        # Metodo Leave One Out
-        k = int(input("Inserisci il valore di k pari al numero di vicini da considerare: "))
-        # Numero di esperimenti richiesto dall'utente:
-        K = int(input(f"Inserisci il numero di esperimenti (intero) tra 1 e {len(X)}: "))
-        Leave_one_out = leave_one_out(K, X) #Inizializzazione
-        metriche = Leave_one_out.evaluate(X,y,k)
-        metriche_da_stampare = metrics_calculator() # Inizializzazione
-        metriche_da_stampare.stampa_metriche(metriche) # Chiamata al metodo che stampa solo le metriche desiderate
-        
-
-elif choice == "3":
-        # Metodo Random Subsampling
-        test_size = float(input("Inserisci il valore percentuale che rappresenta la dimensione del test set rispetto all'intero set di dati: "))
-        k = int(input("Inserisci il valore di k pari al numero di vicini da considerare: "))
-        num_splits = int(input("Inserisci il numero di splits da realizzare nel metodo: "))
-        random_subsampling = RandomSubsampling(test_size, num_splits) #Inizializzazione
-        metriche = random_subsampling.evaluate(X,y,k)
-        
-        metriche_da_stampare = metrics_calculator() # Inizializzazione
-        metriche_da_stampare.stampa_metriche(metriche) # Chiamata al metodo che stampa solo le metriche desiderate
-else:
-        print("Scelta non valida. Riprova.")
+ModelEvaluation = ModelEvaluationFactory().get_validation_strategy(choice, X)
+k = int(input("Inserisci il valore di k pari al numero di vicini da considerare: "))
+metriche = ModelEvaluation.evaluate(X, y, k)
+metriche_da_stampare = metrics_calculator() # Inizializzazione
+metriche_da_stampare.stampa_metriche(metriche) # Chiamata al metodo che stampa solo le metriche desiderate
 
